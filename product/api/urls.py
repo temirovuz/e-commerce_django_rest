f43473af680b9f31ffba4c1eac2e518e2ea837ac
temirovuz <temirov-muhammad@mail.ru>
2024-04-03 15:40:48 +0500

    update product in urls and views files

--- product/api/urls.py
+++ product/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 from category.api.views import CategoryListView
-from product.api.views import ProductListView
+from product.api.views import ProductListView, ProductDetailView
 
 urlpatterns = [
-    path('products/', ProductListView.as_view(), name='category-list')
+    path('products/', ProductListView.as_view(), name='product-list'),
+    path('products/<int:pk>', ProductDetailView.as_view(), name='product-detail')
 ]
--- product/api/views.py
+++ product/api/views.py
@@ -7,3 +7,8 @@ from product.models import Product
 class ProductListView(generics.ListAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.select_related('category').prefetch_related('color', 'size').all()
+
+
+class ProductDetailView(generics.RetrieveAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
