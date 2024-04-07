b60225d72fb1eafec4b02e72ef4dd778840ef3f4
temirovuz <temirov-muhammad@mail.ru>
2024-04-07 10:57:36 +0500

    update services files

--- order/servives.py
+++ order/servives.py
@@ -1,7 +1,7 @@
 from django.db.models import F
 
 
-def inc_or_dec(obj, num,is_inc):
+def inc_or_dec(obj, num, is_inc):
     if is_inc:
         obj.quantity = F('quantity') + num
         obj.save()
